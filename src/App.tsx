02b862e489a6ce2ff88ce765535eb350ecd9a386
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-27 17:24:16 +0800

    Feat: added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
 const App = () => {
    return (
       <>
-         <h1 className="text-3xl">Hello World</h1>
+         <div className="max-w-screen-lg px-4 sm:px-8 lg:px-0 mx-auto">
+            <nav className="flex justify-center">
+               <ul className="bg-neutral-800 flex mt-5 w-fit rounded-lg px-4 py-2 gap-2 text-xs font-medium">
+                  <li>
+                     <a href="https://github.com/ciptacoding" target="_blank">
+                        Github
+                     </a>
+                  </li>
+                  <li>
+                     <a
+                        href="https://www.linkedin.com/in/i-gusti-ngurah-cipta-dwipajaya-9019bb231/"
+                        target="_blank"
+                     >
+                        LinkedIn
+                     </a>
+                  </li>
+                  <li>
+                     <a href="https://twitter.com/ciptadev" target="_blank">
+                        Twitter
+                     </a>
+                  </li>
+                  <li>
+                     <a
+                        href="https://www.instagram.com/ciptadwipajayaa/"
+                        target="_blank"
+                     >
+                        Instagram
+                     </a>
+                  </li>
+               </ul>
+            </nav>
+         </div>
       </>
    );
 };
