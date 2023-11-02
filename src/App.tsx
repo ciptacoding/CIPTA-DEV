10e21a782dc78de837b41d42b520e40955f01e77
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-11-02 19:11:31 +0800

    Feat: added name and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 const App = () => {
    return (
       <>
-         <div className="max-w-screen-lg px-4 sm:px-8 lg:px-0 mx-auto">
+         <section className="max-w-screen-lg px-4 sm:px-8 lg:px-0 mx-auto">
             <nav className="flex justify-center">
-               <ul className="bg-neutral-800 flex mt-5 w-fit rounded-lg px-4 py-2 gap-2 text-xs font-medium">
+               <ul className="bg-neutral-800 flex mt-5 w-fit rounded-lg px-4 py-2 gap-8 text-xs font-medium">
                   <li>
                      <a href="https://github.com/ciptacoding" target="_blank">
                         Github
@@ -32,7 +32,27 @@ const App = () => {
                   </li>
                </ul>
             </nav>
-         </div>
+
+            <main className="mt-10">
+               <div className="flex items-center flex-col">
+                  <div>
+                     <img
+                        src="./src/assets/cipta.png"
+                        alt="Profile"
+                        className="rounded-full aspect-square h-28 object-top object-cover"
+                     />
+                  </div>
+                  <div className="text-center mt-4">
+                     <h1 className="font-extrabold mb-2 text-2xl">
+                        Cipta Dwipajaya
+                     </h1>
+                     <p className="font-bold text-lg">
+                        Web Developer, Software Engineer, Tech Enthusiast.
+                     </p>
+                  </div>
+               </div>
+            </main>
+         </section>
       </>
    );
 };
